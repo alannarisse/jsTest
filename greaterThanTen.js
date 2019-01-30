6fe7d578bd18d09a7ca2e953e2cdff95c960a6a8
alannarisse <angrypirate@gmail.com>
2019-01-30 12:35:21 -0800

    initial commit

---
+++ greaterThanTen.js
@@ -0,0 +1,12 @@
+//greaterThanTen.js
+
+
+//Write a function called "isGreaterThanTen".
+//Given a number, "isGreaterThanTen" returns whether it is greater than 10.
+
+var output = isGreaterThanTen(11);
+console.log(output); // --> true
+
+function isGreaterThanTen(num) {
+  // your code here
+}
